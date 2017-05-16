ac29d537d36b3fbd7e39ba391c59c0acacd57b15
David X. Messer <davx8342@gmail.com>
2017-05-16 22:30:23 +0100

    bug fixes mostly
    if images don't have any cover art a imagemagick generated image is used
    if tv/moviedb doesn't allow that show/movie (eg. Micro Men) this doesn't get included in the contact sheet
    errors regarding no cover art are trapped
    a count of how many shows/movies you have is now in the top navigation
    CSS font tweaks

--- kodisheet.sh
+++ kodisheet.sh
@@ -16,7 +16,7 @@ dbpath="./db"
 #
 # output path to your html
 #
-htmlout="./html"
+htmlout="/var/www/html/kodisheet"
 
 
 #
@@ -39,7 +39,20 @@ fi
 # we save our images in a particular structure, create the structure if any
 # of these directories don't already exist.
 #
-checkdirs="$htmlout/images $htmlout/images/tvshowposters $htmlout/images/movieposters $htmlout/images/tvshowfanart $htmlout/images/moviefanart $htmlout/images/tvshowbanners $htmlout/tvshow $htmlout/movie $htmlout/actor $htmlout/genre"
+checkdirs="$htmlout/images $htmlout/images/tvshowposters $htmlout/images/movieposters $htmlout/images/tvshowfanart $htmlout/images/moviefanart $htmlout/images/tvshowbanners $htmlout/tvshow $htmlout/movie $htmlout/genre"
+
+#
+# some basic metrics
+#
+movieCount=`sqlite3 db/MyVideos107.db "SELECT count() from movie"`
+tvCount=`sqlite3 db/MyVideos107.db "SELECT count() from tvshow"`
+
+#
+# remove the genre index
+#
+if [ -f "$htmlout/genre/index.html" ]; then
+   rm "$htmlout/genre/index.html"
+fi
 
 for dir in $checkdirs; do
    if [ ! -d "$dir" ]; then
@@ -47,6 +60,15 @@ for dir in $checkdirs; do
       mkdir -p $dir
    fi
 done
+
+#
+# if we have files without poster art we need to substitute something or we get broken images
+#
+convert -size 150x221 xc:black $htmlout/images/tvshowbanners/unknown.jpg
+convert -size 150x221 xc:black $htmlout/images/tvshowposters/unknown.jpg
+convert -size 150x221 xc:black $htmlout/images/movieposters/unknown.jpg
+
+
 
 genres=`sqlite3 db/MyVideos107.db "SELECT genre_id from genre"`
 genrenav=""
@@ -78,7 +100,9 @@ done
 
 for genre in $genres; do
    if [ -f "$htmlout/genre/$genre.html" ]; then
+      echo "<font class=\"heading\">" >> $htmlout/genre/$genre.html
       echo "$genrenav" >> $htmlout/genre/$genre.html
+      echo "</font>" >> $htmlout/genre/$genre.html
    fi
 done
 
@@ -119,171 +143,160 @@ for mediatype in $mediatypes; do
    echo "</head><body>" >> $htmlout/$mediatype.html
    echo "<p class=\"navigaton\">" >> $htmlout/$mediatype.html
    echo "<br /><br />" >> $htmlout/$mediatype.html
-   echo "<a href=\"tvshow.html\">TV Shows</a>" >> $htmlout/$mediatype.html
+   echo "<font class=\"giantheading\">" >> $htmlout/$mediatype.html
+   echo "<a href=\"tvshow.html\">TV Shows</a> [$tvCount]" >> $htmlout/$mediatype.html
    echo " | " >> $htmlout/$mediatype.html
-   echo "<a href=\"movie.html\">Movies</a>" >> $htmlout/$mediatype.html
+   echo "<a href=\"movie.html\">Movies</a> [$movieCount]" >> $htmlout/$mediatype.html
    echo " | " >> $htmlout/$mediatype.html
    echo "<a href=\"genre/index.html\">Genres</a>" >> $htmlout/$mediatype.html
-   echo "<br /><br /></p>" >> $htmlout/$mediatype.html
+   echo "<br /><br /></font></p>" >> $htmlout/$mediatype.html
    for idLoop in $idList; do
 
       name=`sqlite3 $dbpath/MyVideos107.db "SELECT c00 from $mediatype where $id=$idLoop"`
-      plot=`sqlite3 $dbpath/MyVideos107.db "SELECT c01 from $mediatype where $id=$idLoop"`
 
-
-
-      if [ "$mediatype" == "tvshow" ]; then
-         status=`sqlite3 $dbpath/MyVideos107.db "SELECT c02 from $mediatype where $id=$idLoop"`
-         rating=`sqlite3 $dbpath/MyVideos107.db "SELECT c04 from $mediatype where $id=$idLoop"`
-         firstaired=`sqlite3 $dbpath/MyVideos107.db "SELECT c05 from $mediatype where $id=$idLoop"`
-         guide=`sqlite3 $dbpath/MyVideos107.db "SELECT c10 from $mediatype where $id=$idLoop"`
-         contentrating=`sqlite3 $dbpath/MyVideos107.db "SELECT c13 from $mediatype where $id=$idLoop"`
-         network=`sqlite3 $dbpath/MyVideos107.db "SELECT c14 from $mediatype where $id=$idLoop"`
-      fi
-
-      if [ "$mediatype" == "movie" ]; then
-         trailerurl=`sqlite3 $dbpath/MyVideos107.db "SELECT c19 from $mediatype where $id=$idLoop"`
-         if [ "$trailerurl" != "" ]; then
-            videoarg=`echo $trailerurl|awk -F"videoid=" '{print $2}'|cut -c1-11`
-            trailerurl="https://www.youtube.com/watch?v=${videoarg}"
-         fi
-         
-         studio=`sqlite3 $dbpath/MyVideos107.db "SELECT c18 from $mediatype where $id=$idLoop"`
-         rating=`sqlite3 $dbpath/MyVideos107.db "SELECT c12 from $mediatype where $id=$idLoop"`
-         runtime=`sqlite3 $dbpath/MyVideos107.db "SELECT c11 from $mediatype where $id=$idLoop"`
-         director=`sqlite3 $dbpath/MyVideos107.db "SELECT c15 from $mediatype where $id=$idLoop"`
-      fi
-
-      echo $name
-
-      out="$htmlout/$mediatype/$idLoop.html"
-      if [ -f "$out" ]; then
-         rm $out
-      fi
-      touch $out
-
-      if [ "$mediatype" == "tvshow" ]; then
-         bannerurl=`sqlite3 $dbpath/MyVideos107.db "SELECT url from art where media_id=$idLoop and type='banner' and media_type=\"$mediatype\""`
-         bannerfile=$(basename $bannerurl)
-         if [ ! -f "$htmlout/images/${mediatype}banners/$bannerfile" ]; then
-            wget -O $htmlout/images/${mediatype}banners/$bannerfile $bannerurl
-         fi
-      fi
-
-#      fanarturl=`sqlite3 $dbpath/MyVideos107.db "SELECT url from art where media_id=$idLoop and type='fanart' and media_type=\"$mediatype\""`
-#      fanartfile=$(basename $fanarturl)
-#      if [ ! -f "$htmlout/images/${mediatype}fanart/$fanartfile" ]; then
-#         wget -O $htmlout/images/${mediatype}fanart/$fanartfile $fanarturl
+      if [ "$name" != "** 403: Series Not Permitted **" ]; then
+#      	echo halt
 #      fi
+         plot=`sqlite3 $dbpath/MyVideos107.db "SELECT c01 from $mediatype where $id=$idLoop"`
 
-      posterurl=`sqlite3 $dbpath/MyVideos107.db "SELECT url from art where media_id=$idLoop and type='poster' and media_type=\"$mediatype\""`
-      posterfile=$(basename $posterurl)
-      if [ ! -f "$htmlout/images/${mediatype}posters/$posterfile" ]; then
-         wget -O $htmlout/images/${mediatype}posters/TEMP${posterfile} $posterurl
-         convert $htmlout/images/${mediatype}posters/TEMP${posterfile} -resize 150 $htmlout/images/${mediatype}posters/${posterfile}
-         if [ -f $htmlout/images/${mediatype}posters/TEMP${posterfile} ]; then
-            rm $htmlout/images/${mediatype}posters/TEMP${posterfile}
+         if [ "$mediatype" == "tvshow" ]; then
+            status=`sqlite3 $dbpath/MyVideos107.db "SELECT c02 from $mediatype where $id=$idLoop"`
+            rating=`sqlite3 $dbpath/MyVideos107.db "SELECT c04 from $mediatype where $id=$idLoop"`
+            firstaired=`sqlite3 $dbpath/MyVideos107.db "SELECT c05 from $mediatype where $id=$idLoop"`
+            guide=`sqlite3 $dbpath/MyVideos107.db "SELECT c10 from $mediatype where $id=$idLoop"`
+            contentrating=`sqlite3 $dbpath/MyVideos107.db "SELECT c13 from $mediatype where $id=$idLoop"`
+            network=`sqlite3 $dbpath/MyVideos107.db "SELECT c14 from $mediatype where $id=$idLoop"`
          fi
-      fi
 
-      #
-      # genres
-      #
-      genres=`sqlite3 $dbpath/MyVideos107.db "SELECT genre_id from genre_link where media_id=$idLoop and media_type=\"$mediatype\""`
-
-      filegenre=""
-      for genre in $genres; do
-         genrename=`sqlite3 $dbpath/MyVideos107.db "SELECT name from genre where genre_id=$genre"`
-#         echo $genrename
-         filegenre="<a href=\"../genre/$genre.html\">$genrename</a> / $filegenre"
-         echo "<a href=\"../$mediatype/$idLoop.html\">" >> $htmlout/genre/$genre.$mediatype
-         echo "<img width=150 src=\"../images/${mediatype}posters/$posterfile\"></a>" >> $htmlout/genre/$genre.$mediatype
-      done
-
-
-
-
-      echo "<a href=\"$mediatype/$idLoop.html\">" >> $htmlout/$mediatype.html
-      echo "<img width=150 src=\"images/${mediatype}posters/$posterfile\"></a>" >> $htmlout/$mediatype.html
-
-      echo "<html><head><title>$name</title>" >> $out
-      echo "<link rel=\"stylesheet\" href=\"../kodisheet.css\" type=\"text/css\">" >> $out
-      echo "</head>" >> $out
-      echo "<body>" >> $out
-      echo "<center><table width=400><tr><td>" >> $out
-
-      if [ "$mediatype" == "tvshow" ]; then
-         echo "<img src=\"../images/${mediatype}banners/$bannerfile\" width=450>" >> $out
-         echo "</p>" >> $out
-         echo "<p class=\"details\">" >> $out
-         echo "Rating: $rating<br />" >> $out
-         echo "First aired: $firstaired<br />" >> $out
-         echo "Genre: $filegenre<br />" >> $out
-         echo "Network: $network<br />" >> $out
-         echo "Content Rating: $contentrating<br />" >> $out
-         echo "</p>" >> $out
-      fi
-      if [ "$mediatype" == "movie" ]; then
-         echo "<p class=\"showtitle\">$name</p>" >> $out
-         echo "<p class=\"artwork\">" >> $out
-         echo "<img src=\"../images/${mediatype}posters/$posterfile\"" >> $out
-         echo "</p>" >> $out
-         echo "<p class=\"details\">" >> $out
-         echo "trailer: <a href=\"$trailerurl\">youtube</a><br />" >> $out
-         echo "studio: $studio<br />" >> $out
-         echo "rating: $rating<br />" >> $out
-         echo "runtime: $runtime<br />" >> $out
-         echo "genre: $filegenre<br />" >> $out
-         echo "director: $director<br />" >> $out
-         echo "</p>" >> $out
-      fi
-
-      echo "<p class=\"synopsis\">" >> $out
-      echo "<font class=\"heading\">Plot synopsis</font><br /><br />" >> $out
-      echo "$plot" >> $out
-      echo "</p>" >> $out
-
-#      echo "<p class=\"cast\">" >> $out
-#      echo "<font class=\"heading\">Cast</font><br /><br />" >> $out
-#      idActors=`sqlite3 $dbpath/MyVideos107.db "SELECT actor_id from actor_link where media_id=$idLoop ORDER BY cast_order"`
-#      for idActor in $idActors; do
-#         actor=`sqlite3 $dbpath/MyVideos107.db "SELECT name from actor where actor_id=$idActor"`
-#         echo "<a href=\"../actor/$idActor.html\">" >> $out
-#         echo "$actor</a>, " >> $out
-#      done
-#
-#      echo "</p>" >> $out
-
-      if [ "$mediatype" == "tvshow" ]; then
-
-         seasons=`sqlite3 $dbpath/MyVideos107.db "SELECT idSeason from seasons where idShow=$idLoop"`
-         for season in $seasons; do
-            count=`sqlite3 $dbpath/MyVideos107.db "SELECT count() from episode where idShow=$idLoop and idSeason=$season"`
-
-            if [ "$count" != "0" ]; then
-               echo "<p class=\"seasonlist\">" >> $out
-               seasonNo=`sqlite3 $dbpath/MyVideos107.db "SELECT season from seasons where idShow=$idLoop and idSeason=$season"`
-               realseason=`sqlite3 $dbpath/MyVideos107.db "SELECT c12 from episode where idShow=$idLoop and idSeason=$season limit 1"`
-               file=`sqlite3 $dbpath/MyVideos107.db "SELECT c18 from episode where idShow=$idLoop and idSeason=$season"`
-               echo "<Font class=\"heading\">Season $realseason</font>" >> $out
-               echo "<br /><br />" >> $out
-
-               episodes=`sqlite3 $dbpath/MyVideos107.db "select idEpisode from episode where idShow=$idLoop and idSeason=$season"`
-
-               for episode in $episodes; do
-                  episodeNo=`sqlite3 $dbpath/MyVideos107.db "SELECT c13 from episode where idShow=$idLoop and idSeason=$season and idEpisode=$episode"`
-                  episodeTitle=`sqlite3 $dbpath/MyVideos107.db "SELECT c00 from episode where idShow=$idLoop and idSeason=$season and idEpisode=$episode"`
-                  echo "$episodeNo. $episodeTitle<br />" >> $out
-               done
-               echo "</p>" >> $out
+         if [ "$mediatype" == "movie" ]; then
+            trailerurl=`sqlite3 $dbpath/MyVideos107.db "SELECT c19 from $mediatype where $id=$idLoop"`
+            if [ "$trailerurl" != "" ]; then
+               videoarg=`echo $trailerurl|awk -F"videoid=" '{print $2}'|cut -c1-11`
+               trailerurl="https://www.youtube.com/watch?v=${videoarg}"
             fi
+         
+            studio=`sqlite3 $dbpath/MyVideos107.db "SELECT c18 from $mediatype where $id=$idLoop"`
+            rating=`sqlite3 $dbpath/MyVideos107.db "SELECT c12 from $mediatype where $id=$idLoop"`
+            runtime=`sqlite3 $dbpath/MyVideos107.db "SELECT c11 from $mediatype where $id=$idLoop"`
+            director=`sqlite3 $dbpath/MyVideos107.db "SELECT c15 from $mediatype where $id=$idLoop"`
+         fi
+
+         echo $name
+
+         out="$htmlout/$mediatype/$idLoop.html"
+         if [ -f "$out" ]; then
+            rm $out
+         fi
+         touch $out
+
+         if [ "$mediatype" == "tvshow" ]; then
+            bannerurl=`sqlite3 $dbpath/MyVideos107.db "SELECT url from art where media_id=$idLoop and type='banner' and media_type=\"$mediatype\""`
+            if [ "$bannerurl" != "" ]; then
+               bannerfile=$(basename $bannerurl)
+               if [ ! -f "$htmlout/images/${mediatype}banners/$bannerfile" ]; then
+                  wget -O $htmlout/images/${mediatype}banners/$bannerfile $bannerurl
+               fi
+            else
+               bannerfile="unknown.jpg"
+            fi
+         fi
+
+         posterurl=`sqlite3 $dbpath/MyVideos107.db "SELECT url from art where media_id=$idLoop and type='poster' and media_type=\"$mediatype\""`
+         if [ "$posterurl" != "" ]; then
+            posterfile=$(basename $posterurl)
+            if [ ! -f "$htmlout/images/${mediatype}posters/$posterfile" ]; then
+               wget -O $htmlout/images/${mediatype}posters/TEMP${posterfile} $posterurl
+               convert $htmlout/images/${mediatype}posters/TEMP${posterfile} -resize 150 $htmlout/images/${mediatype}posters/${posterfile}
+               if [ -f $htmlout/images/${mediatype}posters/TEMP${posterfile} ]; then
+                  rm $htmlout/images/${mediatype}posters/TEMP${posterfile}
+               fi
+            fi
+         else
+            posterfile="unknown.jpg"
+         fi
+
+         #
+         # genres
+         #
+         genres=`sqlite3 $dbpath/MyVideos107.db "SELECT genre_id from genre_link where media_id=$idLoop and media_type=\"$mediatype\""`
+
+         filegenre=""
+         for genre in $genres; do
+            genrename=`sqlite3 $dbpath/MyVideos107.db "SELECT name from genre where genre_id=$genre"`
+#         echo $genrename
+            filegenre="<a href=\"../genre/$genre.html\">$genrename</a> / $filegenre"
+            echo "<a href=\"../$mediatype/$idLoop.html\">" >> $htmlout/genre/$genre.$mediatype
+            echo "<img width=150 src=\"../images/${mediatype}posters/$posterfile\"></a>" >> $htmlout/genre/$genre.$mediatype
          done
 
-         echo "</td></tr></table></center>" >> $out
+         echo "<a href=\"$mediatype/$idLoop.html\">" >> $htmlout/$mediatype.html
+         echo "<img width=150 src=\"images/${mediatype}posters/$posterfile\"></a>" >> $htmlout/$mediatype.html
 
-      fi
+         echo "<html><head><title>$name</title>" >> $out
+         echo "<link rel=\"stylesheet\" href=\"../kodisheet.css\" type=\"text/css\">" >> $out
+         echo "</head>" >> $out
+         echo "<body>" >> $out
+         echo "<center><table width=400><tr><td>" >> $out
 
-   done
+         if [ "$mediatype" == "tvshow" ]; then
+            echo "<img src=\"../images/${mediatype}banners/$bannerfile\" width=450>" >> $out
+            echo "</p>" >> $out
+            echo "<p class=\"details\">" >> $out
+            echo "Rating: $rating<br />" >> $out
+            echo "First aired: $firstaired<br />" >> $out
+            echo "Genre: $filegenre<br />" >> $out
+            echo "Network: $network<br />" >> $out
+            echo "Content Rating: $contentrating<br />" >> $out
+            echo "</p>" >> $out
+         fi
+         if [ "$mediatype" == "movie" ]; then
+            echo "<p class=\"showtitle\">$name</p>" >> $out
+            echo "<p class=\"artwork\">" >> $out
+            echo "<img src=\"../images/${mediatype}posters/$posterfile\"" >> $out
+            echo "</p>" >> $out
+            echo "<p class=\"details\">" >> $out
+            echo "trailer: <a href=\"$trailerurl\">youtube</a><br />" >> $out
+            echo "studio: $studio<br />" >> $out
+            echo "rating: $rating<br />" >> $out
+            echo "runtime: $runtime<br />" >> $out
+            echo "genre: $filegenre<br />" >> $out
+            echo "director: $director<br />" >> $out
+            echo "</p>" >> $out
+         fi
+
+         echo "<p class=\"synopsis\">" >> $out
+         echo "<font class=\"heading\">Plot synopsis</font><br /><br />" >> $out
+         echo "$plot" >> $out
+         echo "</p>" >> $out
+
+         if [ "$mediatype" == "tvshow" ]; then
+
+            seasons=`sqlite3 $dbpath/MyVideos107.db "SELECT idSeason from seasons where idShow=$idLoop"`
+            for season in $seasons; do
+               count=`sqlite3 $dbpath/MyVideos107.db "SELECT count() from episode where idShow=$idLoop and idSeason=$season"`
+
+               if [ "$count" != "0" ]; then
+                  echo "<p class=\"seasonlist\">" >> $out
+                  seasonNo=`sqlite3 $dbpath/MyVideos107.db "SELECT season from seasons where idShow=$idLoop and idSeason=$season"`
+                  realseason=`sqlite3 $dbpath/MyVideos107.db "SELECT c12 from episode where idShow=$idLoop and idSeason=$season limit 1"`
+                  file=`sqlite3 $dbpath/MyVideos107.db "SELECT c18 from episode where idShow=$idLoop and idSeason=$season"`
+                  echo "<Font class=\"heading\">Season $realseason</font>" >> $out
+                  echo "<br /><br />" >> $out
+
+                  episodes=`sqlite3 $dbpath/MyVideos107.db "select idEpisode from episode where idShow=$idLoop and idSeason=$season"`
+
+                  for episode in $episodes; do
+                     episodeNo=`sqlite3 $dbpath/MyVideos107.db "SELECT c13 from episode where idShow=$idLoop and idSeason=$season and idEpisode=$episode"`
+                     episodeTitle=`sqlite3 $dbpath/MyVideos107.db "SELECT c00 from episode where idShow=$idLoop and idSeason=$season and idEpisode=$episode"`
+                     echo "$episodeNo. $episodeTitle<br />" >> $out
+                  done
+                  echo "</p>" >> $out
+               fi
+            done
+            echo "</td></tr></table></center>" >> $out
+         fi
+   fi
+      done
 
    genres=`sqlite3 $dbpath/MyVideos107.db "SELECT genre_id from genre"`
    for genre in $genres; do
@@ -305,4 +318,3 @@ for genre in $genres; do
       echo "</body></html>" >> $htmlout/genre/$genre.html
    fi
 done
-
